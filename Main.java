ef0af80adc1464c179f20d76a5a371167f23abec
lijeje <lije0009@gmail.com>
2022-08-16 00:32:22 +0900

    Add: 추가

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       String s = "aabababa";
+       String part = "aba";
+
+
+        while (s.contains(part)) {
+           s = removeString(s, part);
+        }
+        System.out.println(s);
+    }
+
+
+    public static String removeString(String s, String part){
+
+        s = s.replaceAll(part,"");// abab
+
+        return s;
+    }
+}
---
+++ thirdMax.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class thirdMax {
+
+    public static void main(String []arg){
+        int[] nums = {3,5,2,8,6,2};
+        doSelectionSort(nums);
+//        int result = thirdMax(nums);
+
+    }
+    public static int thirdMax(int[] nums) {
+        int result = 0;
+        int [] sortNums = doSelectionSort(nums);
+        System.out.println(Arrays.toString(sortNums));
+        return result;
+    }
+
+    //선택정렬
+    public static int[] doSelectionSort(int[] arr) {
+        final int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            // 나보다 작은게 있으면 치환
+            if (min != i) {
+                arr[min] = arr[min] + arr[i];
+                arr[i] = arr[min] - arr[i];
+                arr[min] = arr[min] - arr[i];
+            }
+            // 과정 출력
+            System.out.print((i+1)+"번쨰 과정  : ");
+            for(int x : arr)
+                System.out.print(x+" ");
+            System.out.println();
+        }
+        return arr;
+    }
+}
