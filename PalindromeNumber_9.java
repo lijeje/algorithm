88ab3238a09d119cb736b0d573c2d0c9da86c4b8
lijeje <105800795+lijeje@users.noreply.github.com>
2022-09-05 21:58:46 +0900

    Add: 0905

---
+++ PalindromeNumber_9.java
@@ -0,0 +1,37 @@
+package Main;
+
+public class PalindromeNumber_9 {
+
+    public static void main(String []args){
+        //isPalindrome(10);
+
+        boolean test = isPalindrome2(123454321);
+        System.out.println(test);
+    }
+    public static boolean isPalindrome(int x) {
+        int length = (int)Math.log10(x)/2;
+        int maxLength = (int)Math.log10(x);
+        boolean flag = true;
+        String strX = String.valueOf(x);
+
+        if(x<0){
+            return false;
+        }
+
+        for(int i = 0; i<=length; i++){ // 몇번을 도는지 체크
+            char headStr = strX.charAt(i);
+            char tailStr = strX.charAt(maxLength -i);
+            if(headStr != tailStr){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    public static boolean isPalindrome2(int x) {
+        //자바 StringBuilder 랑 Buffer 가 reverse() 메소드 가지고 있는데 이게 안에 있는 문자열을 뒤집어 줌
+        // 그래서 그 문자열하구 정수를 문자열로 변경한거를 비교해주면 한줄로 코드를 끝낼 수 있음.
+        return String.valueOf(x).equals(new StringBuilder().append(x).reverse().toString());
+    }
+}
---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+package Main;
+
+import org.hibernate.mapping.Array;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int nums[] = {2, 7, 11, 15};
+        int result[] = twoSum(nums, 9);
+        System.out.println(result[0] + "," + result[1]);
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
