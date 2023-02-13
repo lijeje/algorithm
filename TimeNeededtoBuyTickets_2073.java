1f646d831db0729a42dd9d7275770f6a6b23e4ee
lije <lije0009@gmail.com>
2023-02-13 23:33:44 +0900

    2073 풀이완료

--- TimeNeededtoBuyTickets_2073.java
+++ TimeNeededtoBuyTickets_2073.java
@@ -6,8 +6,11 @@ import java.util.Queue;
 public class TimeNeededtoBuyTickets_2073 {
 
     public static void main(String[] args) {
-        int[] tickets = {5, 1, 1, 1};
-        int k = 0;
+//        int[] tickets = {5, 1, 1, 1};
+//        int k = 0;
+
+        int[] tickets = {84,49,5,24,70,77,87,8};
+        int k = 3;
 
         TimeNeededtoBuyTickets_2073 timeNeededtoBuyTickets = new TimeNeededtoBuyTickets_2073();
         int answer = timeNeededtoBuyTickets.buyTickets(tickets, k);
@@ -17,24 +20,51 @@ public class TimeNeededtoBuyTickets_2073 {
 
     public int buyTickets(int[] tickets, int k) {
         int ticketCnt = 0;
-        int kcnt = tickets[k];
-
+        int totCnt = 0;
         Queue<Integer> ticketQue = new LinkedList<>();
 
-        for(int i=0; kcnt==1; i++) {
-            int stdKey = tickets[i];
-            if(stdKey > 1) {
-                //뒤로 보내기
-                //전체카운트증가
-            } else {
-                //전체카운트 증가
+        for(int i=0; i<tickets.length;i++){
+            ticketQue.offer(tickets[i]);
+            if(tickets[i]<tickets[k]){
+                totCnt += tickets[i];
+            }else{
+                totCnt +=tickets[k];
             }
         }
-        // 큐길이체크
-        System.out.println(ticketQue);
+        totCnt -= tickets.length-k;
 
+        for(int j=0; totCnt>=1; j++) {
+            int stdKey = ticketQue.poll();
+            if(stdKey > 1) {
+                //뒤로 보내기
+                ticketQue.offer(stdKey);
+                //전체카운트증가
+                ticketCnt +=1;
+                totCnt -=1;
+            } else {
+                //전체카운트 증가, kcnt 감소
+                ticketCnt +=1;
+                totCnt -=1;
+            }
 
-
+        }
         return ticketCnt;
     }
+
+
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int timeTaken = 0;
+        while(tickets[k] > 0) {
+            for(int i = 0; i < tickets.length; i++) {
+                if(tickets[i] > 0) {
+                    tickets[i]--;
+                    timeTaken++;
+                }
+                if(tickets[k] == 0)  break;
+            }
+        }
+
+        return timeTaken;
+    }
+
 }
--- TwoSum.java
+++ TwoSum.java
@@ -1,6 +1,5 @@
 package Main;
 
-import org.hibernate.mapping.Array;
 
 public class TwoSum {
 
