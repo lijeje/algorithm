440688bb22ffd915a306da7c1636e1405881fdab
lijeje <105800795+lijeje@users.noreply.github.com>
2023-02-14 17:54:29 +0900

    설정

--- programmers/Print.java
+++ programmers/Print.java
@@ -1,4 +1,15 @@
 package Main.programmers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Print {
+    public static void main(String[] args) {
+        int[] priorites = {2, 1, 3, 2};
+        int location = 2;
+        int printCnt = 0;
+
+
+        System.out.println(printCnt);
+    }
 }
