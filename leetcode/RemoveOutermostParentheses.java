a2bf130127698b9741a4bacd9d3f64424cfaf591
lijeje <105800795+lijeje@users.noreply.github.com>
2023-02-22 09:45:23 +0900

    RemoveOutermostParentheses 문제만 커밋

---
+++ leetcode/RemoveOutermostParentheses.java
@@ -0,0 +1,22 @@
+package Main.leetcode;
+
+public class RemoveOutermostParentheses {
+    public static void main(String[] args) {
+        RemoveOutermostParentheses removeOutermostParentheses = new RemoveOutermostParentheses();
+
+        String s1 = "(()())(())";//"()()()"
+        String s2 = "(()())(())(()(()))";//"()()()()(())"
+        String s3 = "()()";//""
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+    }
+
+    public String RemoveOutermostParentheses(String s){
+        String str = "";
+
+
+        return str;
+    }
+}
