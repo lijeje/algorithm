a6bda29e0993cc3fc8ba637e9d691bd0a2051e35
lije <lije0009@gmail.com>
2023-03-03 07:52:53 +0900

    릿코드 문제만 커밋

---
+++ leetcode/DeepestLeavesSum_1302.java
@@ -0,0 +1,5 @@
+package Main.leetcode;
+
+public class DeepestLeavesSum_1302 {
+    //https://leetcode.com/problems/deepest-leaves-sum/
+}
---
+++ leetcode/FindaCorrespondingNodeofaBinaryTreeInaCloneofThatTree_1379.java
@@ -0,0 +1,5 @@
+package Main.leetcode;
+
+public class FindaCorrespondingNodeofaBinaryTreeInaCloneofThatTree_1379 {
+    //https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+}
