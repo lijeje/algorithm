e9f8046a9198c3d21002dd2abc846ec19e3bf530
lije <lije0009@gmail.com>
2023-03-06 07:42:57 +0900

    표현가능한 이진트리 문제만 커밋

---
+++ programmers/표현가능한이진트리.java
@@ -0,0 +1,26 @@
+package Main.programmers;
+
+public class 표현가능한이진트리 {
+
+    public static void main(String[] args) {
+        표현가능한이진트리 표현가능한이진트리 = new 표현가능한이진트리();
+        long[] numbers =  {7, 42, 5};
+        System.out.println(표현가능한이진트리.solution(numbers));
+    }
+
+    public int[] solution(long[] numbers) {
+        int[] answer = {};
+        StringBuilder strbuilder = new StringBuilder();
+
+        // 십진수를 이진수로 바꿔주기
+
+
+        // 변형한 이진수로 트리 구현
+        
+        // 트리가 성립하는지 확인 성립한다면 answer에 1, 성립하지않으면 0 넣기
+
+        return answer;
+    }
+
+
+}
