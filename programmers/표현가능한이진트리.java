8f9cdc6607be02bcb4eafc85353644cb5254dba3
lije <lije0009@gmail.com>
2023-03-08 23:49:17 +0900

    표현가능한 이진트리 202308

--- programmers/표현가능한이진트리.java
+++ programmers/표현가능한이진트리.java
@@ -9,26 +9,65 @@ public class 표현가능한이진트리 {
     }
 
     public int[] solution(long[] numbers) {
-        int[] answer = {};
-        StringBuilder strbuilder = new StringBuilder();
+        int[] answer = new int[numbers.length];
+       // String[] binarynumbers = new String[numbers.length]; //이진수로 바꿔준 값을 넣기
+        StringBuilder sb = new StringBuilder();
 
         // 십진수를 이진수로 바꿔주기
-        for(int i=0; i<numbers.length;i++){
-            long number = numbers[i];
-            while(number == 0){
-                strbuilder.append(String.valueOf(number % 2));
-                System.out.println(strbuilder.toString().);
-                if(number == 1) break;
-            }
+//        for(int i=0; i<numbers.length;i++){
+//            long number = numbers[i];
+//            while(number != 0 ){//|| number != 1
+//
+//                sb.append(String.valueOf(number % 2));
+//                number = number/2;
+//                if(number == 1 ||number == 0 ) {
+//                    sb.append(number);
+//                    break;
+//                };
+//            }
+//            binarynumbers[i] = sb.reverse().toString();
+//            sb.setLength(0);
+//        }
 
+        // 함수가 있었군요 ㅇㅅㅇ
+        for(int i = 0; i<numbers.length;i++){
+            String currentStr = Long.toBinaryString(numbers[i]);
+            int j = 0;
+            while((int)Math.pow(2, j)-1 < currentStr.length()) {
+                j++;
+            }
+            while((int)Math.pow(2, j)-1 != currentStr.length()) {
+                currentStr = "0"+ currentStr;
+            }
+            if(dfs(currentStr)) {
+                answer[i] = 1;
+            }
         }
 
 
-        // 변형한 이진수로 트리 구현
-
-        // 트리가 성립하는지 확인 성립한다면 answer에 1, 성립하지않으면 0 넣기
-
         return answer;
+    }
+
+
+    private static boolean dfs(String number) {
+        boolean valid = true;
+
+        int mid = (number.length()-1)/2;
+        char root = number.charAt(mid);
+        String left = number.substring(0,mid);
+        String right = number.substring(mid+1,number.length());
+
+        if(root == '0' && (left.charAt((left.length()-1)/2)=='1' || right.charAt((right.length()-1)/2)=='1')){
+            return false;
+        }
+
+        if(left.length() >= 3) {
+            valid = dfs(left);
+            if(valid) {
+                valid = dfs(right);
+            }
+        }
+        return valid;
     }
 
 
