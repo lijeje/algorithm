612f89e6af87fd7626af73a62728c366b4ba45b2
lije <lije0009@gmail.com>
2023-03-21 07:46:40 +0900

    targetNumber Question commit

---
+++ programmers/TargetNumber.java
@@ -0,0 +1,20 @@
+package Main.programmers;
+
+public class TargetNumber {
+
+    public static void main(String[] args) {
+        int [] numbers = {1, 1, 1, 1, 1};
+        int [] numbers2 = {4, 1, 2, 1};
+        int target = 3;
+        int target2 = 4;
+        TargetNumber targetNumber = new TargetNumber();
+        targetNumber.solution(numbers, target);
+    }
+
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
