43594c1aec6337c71f731942cfa067982fe4f5fc
lije <lije0009@gmail.com>
2023-03-22 08:08:22 +0900

    targetNumber Solution commit

--- programmers/TargetNumber.java
+++ programmers/TargetNumber.java
@@ -1,7 +1,7 @@
 package Main.programmers;
 
 public class TargetNumber {
-
+    int answer = 0;
     public static void main(String[] args) {
         int [] numbers = {1, 1, 1, 1, 1};
         int [] numbers2 = {4, 1, 2, 1};
@@ -12,9 +12,25 @@ public class TargetNumber {
     }
 
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+
+        int depth = 0;
+        int sum = 0;
+        //DFS방식으로 풀어볼까ㅏ 그렇다면 재귀호출?
+        DFS(numbers,target,depth,sum);
+
+
 
 
         return answer;
     }
-}
+
+    public void DFS(int[] numbers, int target,int depth,int sum){
+        if(numbers.length == depth){
+            if(sum == target) answer++;
+        }else {
+            DFS(numbers,target,depth+1,sum+numbers[depth]);
+            DFS(numbers,target,depth+1,sum-numbers[depth]);
+        }
+
+    }
+ }
